e10bce2bbdf8fe8c915699c4085e623141a57878
szymon-kw <szymonkwiecien7@wp.pl>
2025-03-11 19:57:48 +0100

    Initial commit

---
+++ src/main/java/pl/error_handling_app/ErrorHandlingAppApplication.java
@@ -0,0 +1,13 @@
+package pl.error_handling_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorHandlingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorHandlingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/error_handling_app/ErrorHandlingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.error_handling_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorHandlingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
