fe68aee8127a774aed7d9c3004f416615be35dcb
szymon-kw <szymonkwiecien7@wp.pl>
2025-03-24 13:43:23 +0100

    Add BCrypt password encoder

--- src/main/resources/db/testdata/002_users_roles.sql
+++ src/main/resources/db/testdata/002_users_roles.sql
@@ -1,12 +1,14 @@
 INSERT INTO users (first_name, last_name, email, password, company_id) VALUES
-('Jan', 'Kowalski', 'jan.kowalski@example.com', '{noop}haslo123', 1),
-/* PRACOWNIK */
-('Anna', 'Nowak', 'anna.nowak@example.com', '{noop}bezpieczneHaslo!', 2),
-/* ADMIN */
-('Piotr', 'Wiśniewski', 'admin@example.com', '{noop}tajneHaslo1', 3),
-('Maria', 'Dąbrowska', 'maria.dabrowska@example.com', '{noop}mojeSuperHaslo', 4),
-/* PRACOWNIK */
-('Tomasz', 'Lewandowski', 'tomasz.lewandowski@example.com', '{noop}1234pass', 5);
+ /* haslo123 */
+('Jan', 'Kowalski', 'jan.kowalski@example.com', '$2a$12$nvsws8lbeeYNlQz.U0zSuuk5TbZiyLsUq3Qd9Meg8DLl6/2b4bx/K', 1),
+/* PRACOWNIK,  bezpieczneHaslo! */
+('Anna', 'Nowak', 'anna.nowak@example.com', '$2a$12$3N0FXP7.Vg2cJPhvLDO8jeM3jK84CHh8QphpBVsgotWvHgtZ7fB9C', 2),
+/* ADMIN,  tajneHaslo1 */
+('Piotr', 'Wiśniewski', 'admin@example.com', '$2a$12$tezxXugfvQ1FNxrAHwAlO.1f6WI9pRGY167/WkSjigQFAH7jjQV1K', 3),
+/* testowehaslo123 */
+('Maria', 'Dąbrowska', 'maria.dabrowska@example.com', '$2a$12$rolAqIClNICoNNxLpQWhKu6/E3xxuOjf7ZwZKvTSFPcg696EYe/cu', 4),
+/* PRACOWNIK, 1234pass */
+('Tomasz', 'Lewandowski', 'tomasz.lewandowski@example.com', '$2a$12$pVi.Ev6a1AY4/jyOuU/eoOR4JL8h4dWMMsmlVGIjC3pop7GFWRZZu', 5);
 
 INSERT INTO user_role (name, description) VALUES
 ('USER', 'Zwykły użytkownik z podstawowymi uprawnieniami'),
